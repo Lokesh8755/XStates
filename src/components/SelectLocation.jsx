3bd2c50e21e2317a17867e91fd8cfd6bbf867eee
Lokesh Singh Mehta <lokeshsinghmehta686@gmail.com>
2024-12-23 15:32:06 +0530

    rephrased to old code

--- src/components/SelectLocation.jsx
+++ src/components/SelectLocation.jsx
@@ -18,59 +18,50 @@ const SelectLocation = ({
     <div>
       <h1 className="heading">Select Location</h1>
 
-      {/* Country Dropdown */}
-      <select
-        id="countryDropdown"
-        className="selectInputs"
-        value={selectedCountries}
-        onChange={(e) => setSelectedCountries(e.target.value)}
+       {/* Country Dropdown */}
+      <select id="countryDropdown"
+              className="selectInputs"
+              value={selectedCountries}
+              onChange={(e) => setSelectedCountries(e.target.value)}
       >
-        <option value="">Select Country</option>
+
+        <option value="">Select Country</option> 
         {countries.map((country) => (
-          <option key={country} value={country}>
-            {country}
-          </option>
+            <option key={country} value={country}>{country}</option>
         ))}
       </select>
-
+      
       {/* State Dropdown */}
-      <select
-        id="stateDropdown"
-        className="selectInputs"
-        value={selectedState}
-        onChange={(e) => setSelectedState(e.target.value)}
-        disabled={!isStateDropdownEnabled}
+      <select 
+      id="stateDropdown" 
+      className="selectInputs"
+      value={selectedState}
+      onChange={(e) => setSelectedState(e.target.value)}
+      disabled={!isStateDropdownEnabled}
       >
-        <option value="">Select State</option>
+        <option value="option1">Select State</option> 
         {states.map((state) => (
-          <option key={state} value={state}>
-            {state}
-          </option>
+            <option key={state} value={state}>
+                {state}
+            </option>
         ))}
       </select>
 
       {/* City Dropdown */}
-      <select
-        id="cityDropdown"
-        className="selectInputs"
-        value={selectedCity}
-        onChange={(e) => setSelectedCity(e.target.value)}
-        disabled={!isCityDropdownEnabled}
+      <select 
+      id="cityDropdown" 
+      className="selectInputs"
+      value={selectedCity}
+      onChange={(e) => setSelectedCity(e.target.value)}
+      disabled={!isCityDropdownEnabled}
       >
-        <option value="">Select City</option>
+        <option value="option1">Select City</option> 
         {cities.map((city) => (
-          <option key={city} value={city}>
-            {city}
-          </option>
+            <option key={city} value={city}>
+                {city}
+            </option>
         ))}
       </select>
-
-      {/* Display Selected Location */}
-      <div className="selectedLocationDisplay">
-        {selectedCountries && <p>Country: {selectedCountries}</p>}
-        {selectedState && <p>State: {selectedState}</p>}
-        {selectedCity && <p>City: {selectedCity}</p>}
-      </div>
     </div>
   )
 }
